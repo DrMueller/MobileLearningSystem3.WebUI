f99ddb62187e7788dbc5696a1553fd40cd05150a
Matthias Müller <matthias.mueller@trivadis.com>
2019-10-30 14:32:34 +0100

    WIP

--- src/app/shell/app-state/app-state.module.ts
+++ src/app/shell/app-state/app-state.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 
 import { securityReducer } from '../security/state';
 
@@ -23,6 +25,10 @@ const reducers: ActionReducerMap<IAppState> = {
         // strictStateImmutability: true,
         // strictActionImmutability: true,
       },
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
     }),
     EffectsModule.forRoot([]),
   ]
